d0a78a8de92d52fecbbb149a36dd8ecf234e5de7
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-04-11 23:21:39 -0400

    second gallery completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector("ul.gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item ">
+      <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+     alt="${description}"
+      />
+      </a>
+    </li>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+const onModalWindow = (evt) => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  new SimpleLightbox(".gallery__item a", {
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+};
+
+galleryEl.addEventListener("click", onModalWindow);
